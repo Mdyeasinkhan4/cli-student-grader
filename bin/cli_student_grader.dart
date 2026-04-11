b580b1f2a3b6b2a1422ae1388b6d1e0fb0ff1071
Mdyeasinkhan4 <mdyeasinkhan1001@gmail.com>
2026-04-11 14:30:00 +0600

    Added AppTitle using const, subjects using final, List including Map.Added 3 students data and printed the menu page.

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,5 +1,52 @@
 import 'package:cli_student_grader/cli_student_grader.dart' as cli_student_grader;
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli_student_grader.calculate()}!');
+void main() {
+  const String App_Title = "Student Grader v1.0";
+
+  final Set<String> availableSubjects = {
+    "Math",
+    "English",
+    "Science",
+    "Islam"
+  };
+
+  List<Map<String, dynamic>> students = [{
+    "Name"   : "Yeasin",
+    "Scores" : [89, 91, 81],
+    "Subjects": {...availableSubjects},
+    "bonus" : null,
+    "comment" : null
+    },
+    {
+    "Name"   : "Fatima",
+    "Scores" : [85, 92, 78],
+    "Subjects": {...availableSubjects},
+    "bonus" : 5,
+    "comment" : "Good progress"
+    },
+    {
+    "Name"   : "Rahim",
+    "Scores" : [60, 70, 65],
+    "Subjects": {...availableSubjects},
+    "bonus" : null,
+    "comment" : null
+    }
+  ];
+
+print('''===== $App_Title =====
+
+1. Add Student
+2. Record Score
+3. Add Bonus Points
+4. Add Comment
+5. View All Students
+6. View Report Card
+7. Class Summary
+8. Exit
+
+Choose an option: ''');
+
+
+
 }
+
