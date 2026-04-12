a187c3172ce630e0581bf3a68f773bedb8c57967
Mdyeasinkhan4 <mdyeasinkhan1001@gmail.com>
2026-04-12 17:30:48 +0600

    Added bonus section

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -27,7 +27,7 @@ void main() {
       "comment": "Good progress"
     },
     {
-      "Name": "Rahim",
+      "Name": "Akash",
       "Scores": [60, 70, 65],
       "Subjects": {...availableSubjects},
       "bonus": null,
@@ -35,11 +35,10 @@ void main() {
     }
   ];
 
-//var isRunning = true;
+var isRunning = true;
 
-// do{};
-
-// Main Menu
+do{
+  // Main Menu
   print('''===== $App_Title =====
 
 1. Add Student
@@ -136,5 +135,53 @@ Choose an option: ''');
       (student["scores"] as List<int>).add(score);
       print("Score $score added for $subject.\n");
       break;
+  //Bonus Add
+  case 3:
+      if (students.isEmpty) {
+        print("No students available.\n");
+        break;
+      }
+
+      int index;
+      while (true) {
+        for (int i = 0; i < students.length; i++) {
+          print("${i + 1}. ${students[i]["name"]}");
+        }
+
+        print("Select student:");
+        var input = stdin.readLineSync();
+
+        var num = int.tryParse(input ?? "");
+        if (num != null && num >= 1 && num <= students.length) {
+          index = num - 1;
+          break;
+        }
+
+        print("Invalid student number!");
+      }
+
+      var student = students[index];
+      int bonus = 0;
+        while (bonus < 1 || bonus > 10) {
+          print("Enter bonus (1–10): ");
+          var input = stdin.readLineSync();
+
+          if (input == null || int.tryParse(input) == null) {
+            print("Invalid number!");
+            continue;
+          }
+
+          bonus = int.parse(input);
+        }
+
+        if (student["bonus"] == null) {
+          student["bonus"] ??= bonus;
+          print("Bonus added!\n");
+        } else {
+          print("Bonus already exists!\n");
+        }
+        break;
+
   }
+}while(isRunning);
 }
